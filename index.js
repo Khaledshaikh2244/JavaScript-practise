f05255b2f27ed0f5fdc93789a946c3c8b9a91cd7
zaheer2400 <shaikhzaheer2400@gmail.com>
2023-11-18 23:40:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+function getFirstElenent(arr) {
+    return arr[0];
+}
+let ans = getFirstElenent([1, 2, 3]);
+let ans2 = getFirstElenent(["one", "two", "Three"]);
+console.log(ans);
+console.log(ans2);
+
+
